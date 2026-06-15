fe332dd0795a98e79c80fa0b264178687748179f
Kireway <erik.silva@germinare.org.br>
2026-06-15 13:18:02 -0300

    Initial commit

---
+++ src/main/java/com/example/locusverse/LocusverseApplication.java
@@ -0,0 +1,13 @@
+package com.example.locusverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocusverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocusverseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/locusverse/LocusverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.locusverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocusverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
